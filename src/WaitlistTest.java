f4848d522a5bc0d373bbc9474e03b694ab29651a
Augy Markham <katiefaithmarkham@gmail.com>
2025-01-09 14:51:54 -0800

    started building tests

--- src/WaitlistTest.java
+++ src/WaitlistTest.java
@@ -1,11 +1,18 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class WaitlistTest {
   
   // TODO: Implement tests for removeStudents
+  @Test
+  void removeStudentsBasic() {
+    Set<String> removeSet = new HashSet<>(Set.of("r", "m"));
+    Waitlist waitlist = new Waitlist;
+  } 
 
   // Hints:
   // - removeStudents is an instance method, so your arrange will need to create an instance of Waitlist
