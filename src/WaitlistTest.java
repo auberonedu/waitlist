b627dec37536fe01f584c54678dfc4d749723f4b
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-01-09 15:14:00 -0800

    Updated test 3 and added test 4

--- src/WaitlistTest.java
+++ src/WaitlistTest.java
@@ -8,25 +8,38 @@ import java.util.Set;
 public class WaitlistTest {
   
   // DONE: Implement tests for removeStudents
+
+  // Test 1: Removing the specific students
   @Test
   void testRemoveSpecificStudents() {
-    // Test 1: Removing the specific students
     Waitlist waitlist = new Waitlist(new String[]{"x", "r", "q", "m", "v", null, null}, 7);
     waitlist.removeStudents(new HashSet<>(Set.of("r", "m")));
     assertArrayEquals(new String[]{"x", "q", "v", null, null, null, null}, waitlist.getWaitlist());
-
-    // Test 3: Removing all the students
-    waitlist = new Waitlist(new String[]{"z", "b", "a", "w", "q", null, null}, 7);
-    waitlist.removeStudents(new HashSet<>(Set.of("z", "b", "a", "w", "q")));
-    assertArrayEquals(new String[]{null, null, null, null, null, null, null}, waitlist.getWaitlist());
   }
 
+  // Test 2: Not removing any students 
   @Test
   void testNotRemovingAnyStudents() {
-    // Test 2: Not removing any students 
     Waitlist waitlist = new Waitlist(new String[]{"a", "b", "c", "d", "e", null, null}, 7);
     waitlist.removeStudents(new HashSet<>());
     assertArrayEquals(new String[]{"a", "b", "c", "d", "e", null, null}, waitlist.getWaitlist());
+  }
+
+  // Test 3: Removing all the students
+  @Test
+  void testRemoveAllStudents() {
+    Waitlist waitlist = new Waitlist(new String[]{"z", "b", "a", "w", "q", null, null}, 7);
+    waitlist.removeStudents(new HashSet<>(Set.of("z", "b", "a", "w", "q")));
+    assertArrayEquals(new String[]{null, null, null, null, null, null, null}, waitlist.getWaitlist());
+
+  }
+
+  // Test 4: Trying to remove elements from an Empty Students Array
+  @Test 
+  void testNoStudentsArray() {
+    Waitlist waitlist = new Waitlist(new String[]{null, null, null, null, null}, 5);
+    waitlist.removeStudents(new HashSet<>(Set.of("a", "b")));
+    assertArrayEquals(new String[]{null, null, null, null, null}, waitlist.getWaitlist());
   }
 
   // Hints:
