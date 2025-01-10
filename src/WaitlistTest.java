2a9cb77e510270a5b93e536879c244d11bd863b9
mmaslov007 <maslov.maxwell@student.greenriver.edu>
2025-01-09 17:48:48 -0800

    Completed removeStudents method and added a test case for standard functionality

--- src/WaitlistTest.java
+++ src/WaitlistTest.java
@@ -1,14 +1,32 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import java.util.Arrays;
-
+import java.util.HashSet;
+import java.util.Set;
 
 public class WaitlistTest {
-  
+
   // TODO: Implement tests for removeStudents
 
   // Hints:
-  // - removeStudents is an instance method, so your arrange will need to create an instance of Waitlist
-  // - When comparing arrays you should use Arrays.equals instead of the equals instance method or ==
-  //     java.util.Arrays is imported for you at the top of this file. 
+  // - removeStudents is an instance method, so your arrange will need to create
+  // an instance of Waitlist
+  // - When comparing arrays you should use Arrays.equals instead of the equals
+  // instance method or ==
+  // java.util.Arrays is imported for you at the top of this file.
+
+  @Test
+  void testRemoveStudents() {
+    String[] students = { "Huma", "Brian", "Smith", "John", "Elizabeth", null, null };
+    Waitlist waitlist = new Waitlist(students, 7);
+
+    Set<String> toRemove = new HashSet<>();
+    toRemove.add("Smith");
+    toRemove.add("John");
+
+    waitlist.removeStudents(toRemove);
+
+    String[] expectedWaitlist = { "Huma", "Brian", "Elizabeth", null, null, null, null };
+    assertArrayEquals(expectedWaitlist, waitlist.getWaitlist());
+  }
+
 }
