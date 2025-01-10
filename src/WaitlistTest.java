2864b77d857024ae636f7078946088d3b32c47ad
Alexander Ruban <alexruban29@gmail.com>
2025-01-10 10:21:08 -0800

    Added testRemoveAllStudents test to WaitlistTest.java

--- src/WaitlistTest.java
+++ src/WaitlistTest.java
@@ -4,18 +4,19 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-
 public class WaitlistTest {
-  
+
   // TODO: Implement tests for removeStudents
 
   // Hints:
-  // - removeStudents is an instance method, so your arrange will need to create an instance of Waitlist
-  // - When comparing arrays you should use Arrays.equals instead of the equals instance method or ==
-  //     java.util.Arrays is imported for you at the top of this file. 
+  // - removeStudents is an instance method, so your arrange will need to create
+  // an instance of Waitlist
+  // - When comparing arrays you should use Arrays.equals instead of the equals
+  // instance method or ==
+  // java.util.Arrays is imported for you at the top of this file.
   @Test
   void testRemoveStudents() {
-    String[] testArr = {"x", "y", "z", "a", "b", "c"};
+    String[] testArr = { "x", "y", "z", "a", "b", "c" };
 
     Waitlist testWaitlist = new Waitlist(testArr, 8);
 
@@ -25,20 +26,32 @@ public class WaitlistTest {
 
     testWaitlist.removeStudents(toRemove);
 
-    String[] expectedArr = {"y", "a", "b", "c", null, null, null, null};
+    String[] expectedArr = { "y", "a", "b", "c", null, null, null, null };
 
     assertTrue(Arrays.equals(expectedArr, testWaitlist.getWaitlist()));
   }
 
   @Test
   void testRemoveStudentsWithNumbers() {
-    Waitlist list = new Waitlist(new String[]{"m", "2", "x", "z", "67", "a", "25", "2"}, 8);
+    Waitlist list = new Waitlist(new String[] { "m", "2", "x", "z", "67", "a", "25", "2" }, 8);
     Set<String> set = new HashSet<>();
     set.add("25");
     set.add("67");
     list.removeStudents(set);
 
-    assertArrayEquals(new String[]{"m", "2", "x", "z", "a", "2", null, null}, list.getWaitlist());
+    assertArrayEquals(new String[] { "m", "2", "x", "z", "a", "2", null, null }, list.getWaitlist());
   }
 
+  @Test
+  void testRemoveAllStudents() {
+    Waitlist list = new Waitlist(new String[] { "a", "b", "c", "d" }, 4);
+    Set<String> set = new HashSet<>();
+    set.add("a");
+    set.add("b");
+    set.add("c");
+    set.add("d");
+    list.removeStudents(set);
+
+    assertArrayEquals(new String[] { null, null, null, null }, list.getWaitlist());
+  }
 }
