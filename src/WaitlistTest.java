30b665c662d639c4d47a4244d30b3a780f29b289
alstondsouza1 <alstondsouza35@gmail.com>
2025-01-12 22:19:34 -0800

    separated tests for clarity

--- src/WaitlistTest.java
+++ src/WaitlistTest.java
@@ -13,26 +13,53 @@ public class WaitlistTest {
   // - When comparing arrays you should use Arrays.equals instead of the equals instance method or ==
   //     java.util.Arrays is imported for you at the top of this file.
   
+  // @Test
+  // public void testRemoveAllStudents() {
+  //   String[] initialIds = {"x", "r", "q", "m", "v"};
+  //   Waitlist waitlist = new Waitlist(initialIds, 7);
+
+  //   // Case 1: Remove some students
+  //   waitlist.removeStudents(new HashSet<>(Arrays.asList("r", "m")));
+  //   String[] expected = {"x", "q", "v", null, null, null, null};
+  //   assertArrayEquals(expected, waitlist.getWaitlist());
+
+  //   // Case 2: Remove a student not in the list
+  //   waitlist.removeStudents(new HashSet<>(Arrays.asList("z")));
+  //   assertArrayEquals(expected, waitlist.getWaitlist());
+
+  //   // Case 3: Remove all students
+  //   waitlist.removeStudents(new HashSet<>(Arrays.asList("x", "q", "v")));
+  //   String[] expectedEmpty = {null, null, null, null, null, null, null};
+  //   assertArrayEquals(expectedEmpty, waitlist.getWaitlist());
+
+  //   // Case 4: Remove from all already empty waitlist
+  // }
+
+  @Test
+  public void testRemoveSomeStudents() {
+    String[] initialIds = {"x", "r", "q", "m", "v"};
+    Waitlist waitlist = new Waitlist(initialIds, 7);
+    waitlist.removeStudents(new HashSet<>(Arrays.asList("r", "m")));
+    String[] expected = {"x", "q", "v", null, null, null, null};
+    assertArrayEquals(expected, waitlist.getWaitlist());
+  }
+
+  @Test
+  public void testRemoveStudentsNotInList() {
+    String[] initialIds = {"x", "r", "q", "m", "v"};
+    Waitlist waitlist = new Waitlist(initialIds, 7);
+    waitlist.removeStudents(new HashSet<>(Arrays.asList("z")));
+    String[] expected = {"x", "r", "q", "m", "v", null, null};
+    assertArrayEquals(expected, waitlist.getWaitlist());
+  }
+
   @Test
   public void testRemoveAllStudents() {
     String[] initialIds = {"x", "r", "q", "m", "v"};
     Waitlist waitlist = new Waitlist(initialIds, 7);
-
-    // Case 1: Remove some students
-    waitlist.removeStudents(new HashSet<>(Arrays.asList("r", "m")));
-    String[] expected = {"x", "q", "v", null, null, null, null};
-    assertArrayEquals(expected, waitlist.getWaitlist());
-
-    // Case 2: Remove a student not in the list
-    waitlist.removeStudents(new HashSet<>(Arrays.asList("z")));
-    assertArrayEquals(expected, waitlist.getWaitlist());
-
-    // Case 3: Remove all students
-    waitlist.removeStudents(new HashSet<>(Arrays.asList("x", "q", "v")));
+    waitlist.removeStudents(new HashSet<>(Arrays.asList("x", "r", "q", "m", "v")));
     String[] expectedEmpty = {null, null, null, null, null, null, null};
     assertArrayEquals(expectedEmpty, waitlist.getWaitlist());
-
-    // Case 4: Remove from all already empty waitlist
   }
 
   // Testing to see if the program doesn't have to remove any students.
